6b160b22a463de2de6ea50a017d384814a2cf661
Cristian <cadrianocondor34@gmail.com>
2025-09-11 15:11:20 -0500

    tabla imagen

--- Backend/src/main/java/biocampo/demo/Persistance/Entity/Imagen.java
+++ Backend/src/main/java/biocampo/demo/Persistance/Entity/Imagen.java
@@ -6,6 +6,8 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,6 +31,7 @@ public class Imagen {
     @Lob
     @Column(nullable = false, length = 500)
     private String url;
+    @Enumerated(EnumType.STRING)
     private TipoEntidad tipoEntidad;
     private Long idReferencia;
     @CreationTimestamp
