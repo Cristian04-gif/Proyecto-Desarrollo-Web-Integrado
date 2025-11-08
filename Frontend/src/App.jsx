5f1021707e98125f49aac1575d9d7c35395be1b8
Cristian <cadrianocondor34@gmail.com>
2025-11-08 18:47:40 -0500

    mercado pago v2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MercadoPago from '../my-mercadopago-app/src/App.js'
+import MercadoPago from './Components/MercadoPago.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
